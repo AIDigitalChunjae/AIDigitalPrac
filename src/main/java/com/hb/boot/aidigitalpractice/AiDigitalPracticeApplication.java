25bec0047c2e07f3f9f71c32105e34e3d7448972
heebum kim <heebum9955@naver.com>
2025-07-23 11:04:29 +0900

    initial Commit

---
+++ src/main/java/com/hb/boot/aidigitalpractice/AiDigitalPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hb.boot.aidigitalpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDigitalPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDigitalPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hb/boot/aidigitalpractice/AiDigitalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.boot.aidigitalpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDigitalPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
